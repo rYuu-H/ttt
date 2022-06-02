49495178afae897fa200d065642e8c75091901f6
rYuu- <holungsang@yahoo.com>
2022-06-02 23:35:28 +0800

    added win condition and random bot

--- ttt.py
+++ ttt.py
@@ -1,6 +1,7 @@
 import turtle
+import random
 tur = turtle.Turtle()
-tur.color('red')
+tur.color('black')
 tur.speed(0)
 tur.hideturtle()
 tur.up()
@@ -18,11 +19,13 @@ def drawline(a,b):
     
 def drawshape(game,x,y):
     if game.turn=="o":
+        tur.color('red')
         tur.goto(-game.x*game.size/2-game.size/2+game.size*(y),game.y*game.size/2-game.size*0.9-game.size*(x-1))
         tur.down()
         tur.circle(game.size*0.4)
         tur.up()
     else:
+        tur.color('blue')
         tur.goto(-game.x*game.size/2+game.size*0.1+game.size*(y-1),game.y*game.size/2-game.size*0.1-game.size*(x-1))
         tur.down()
         tur.goto(-game.x*game.size/2+game.size*0.9+game.size*(y-1),game.y*game.size/2-game.size*0.9-game.size*(x-1))
@@ -42,25 +45,76 @@ class game():
         self.player2="x"
         self.turn="o"
         self.end=False
+        self.connect = 5
         self.winner=""
         drawboard(self)
- //hi
-
+ 
 def move(game,x,y):
     if game.board[x-1][y-1]==0:
         game.board[x-1][y-1]=game.turn    
         drawshape(game,x,y)
+        game.end = check(game,x,y)
+        if game.end == True:
+            game.winner=game.turn
+            print([x,y])
         if game.turn=="o":
             game.turn="x"
         else:
             game.turn="o"
         
+def check(game,x,y):
+    top=[-1,0]
+    bot=[1,0]
+    left=[0,-1]
+    right=[0,1]
+    topleft=[-1,-1]
+    botright=[1,1]
+    topright=[-1,1]
+    botleft=[1,-1]
+    connected=1
+    coun=0
+    direct=[top,bot,left,right,topleft,botright,topright,botleft]
+    for i in direct:
+        t = 1
+        try:
+            while game.board[x-1+i[0]*t][y-1+i[1]*t] == game.turn and 0 <= x-1+i[0]*t<=game.x and 0 <= y-1+i[1]*t<=game.x  :
+                connected=connected+1
+                t=t+1
+        except:
+            t=t
+        if connected >= game.connect:
+            print(connected)
+            return True  
+        coun=coun+1
+        
+        if coun == 2:
+            coun = 0
+            print(connected)
+            connected=1
+    return False  
 
-game1 = game(3,3) 
+def ranbot(game,turn):
+    while game.turn == turn:
+        botx=random.randint(1,game.x)
+        boty=random.randint(1,game.y)
+        move(game,botx,boty)
+    print([botx,boty])
+
+
+
+game1 = game(10,10)
+ 
 while game1.end == False:
-    i=input()
-    x = int(i.split()[0])
-    y = int(i.split()[1])
-    move(game1,x,y)
-    print(game1.board)
-
+    # i=input()
+    # x = int(i.split()[0])
+    # y = int(i.split()[1])
+    # move(game1,x,y)
+    ranbot(game1,game1.turn)
+while game1.end == False:
+    # i=input()
+    # x = int(i.split()[0])
+    # y = int(i.split()[1])
+    # move(game1,x,y)
+    ranbot(game1,game1.turn)
+print(game1.winner," win")
+input()
